818b2548c99b563314e1a7c52d0a9604542f8b49
Adem Onur Çelik <ademonurcelik@icloud.com>
2023-06-27 13:33:30 +0300

    Add CORS policy

--- ECommerceAPI.API/Program.cs
+++ ECommerceAPI.API/Program.cs
@@ -2,6 +2,16 @@ using ECommerceAPI.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddPersistence();
 
 builder.Services.AddControllers();
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
